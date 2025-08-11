d64c306e86ee01f85a42a0eab6db931786e3ad15
kamranghyan-nbs <kamran.ghyan.c@propertyfinder.ae>
2025-08-11 12:50:26 +0500

    Web Concepts : axum, routes, handlers, app state, sqlx

---
+++ web_concepts/axum/playing_axum/src/main.rs
@@ -0,0 +1,100 @@
+use axum::{
+    body::Bytes,
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::Serialize;
+use sqlx::{Pool, Sqlite, SqlitePool};
+use std::{env, net::SocketAddr, sync::Arc};
+use uuid::Uuid;
+
+#[derive(Serialize)]
+struct Message {
+    message: String,
+}
+
+#[derive(Serialize)]
+struct Note {
+    id: Uuid,
+    content: String,
+}
+
+type Conn = Arc<Pool<Sqlite>>;
+
+#[tokio::main]
+async fn main() {
+    let pool = SqlitePool::connect("sqlite:data.db")
+        .await
+        .unwrap();
+
+    let state_pool = Arc::new(pool);
+
+    let app = Router::new()
+        .route("/note", post(create_note))
+        .route("/note/:id", get(get_note))
+        .with_state(state_pool);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// TODO: generalise state rather than lazy static
+async fn create_note(State(pool): State<Conn>, body: Bytes) -> impl IntoResponse {
+    let id = Uuid::new_v4();
+    let body_content: Vec<u8> = body.into_iter().collect();
+    // TODO: handle error
+    let body_content_str = String::from_utf8(body_content).unwrap();
+
+    // TODO: handle error properly
+    //let mut conn = pool.acquire().await.unwrap();
+
+    sqlx::query(
+        r#"
+    INSERT INTO notes ( id, content )
+    VALUES ( ?1, ?2 )
+            "#,
+    )
+    .bind(id.to_string().as_str())
+    .bind(body_content_str.clone())
+    .execute(&(*pool))
+    .await
+    .unwrap();
+
+    let new_note = Note {
+        id,
+        content: body_content_str,
+    };
+
+    (StatusCode::CREATED, Json(new_note))
+}
+
+async fn get_note(State(pool): State<Conn>, Path(id): Path<Uuid>) -> impl IntoResponse {
+    let _id = id.to_string();
+    let row = sqlx::query!("SELECT content FROM notes WHERE id = ?", _id)
+        .fetch_optional(&(*pool))
+        .await
+        .unwrap();
+
+    if row.is_none() {
+        let error = Message {
+            message: "Not found".to_string(),
+        };
+        let error: serde_json::Value = serde_json::to_value(error).unwrap();
+        return (StatusCode::NOT_FOUND, Json(error));
+    }
+
+    let content: &str = &row.unwrap().content;
+
+    let note = Note {
+        id,
+        content: content.to_string(),
+    };
+    let note: serde_json::Value = serde_json::to_value(note).unwrap();
+    (StatusCode::OK, Json(note))
+}
